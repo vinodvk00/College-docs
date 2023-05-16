7885befd8317ec1d2ec02a7d71c3278edf70b248
Vinod <118246607+vinodvk00@users.noreply.github.com>
2023-05-16 12:21:42 +0530

    Create AnimalMain.java

---
+++ Oops/lab/week6/AnimalMain.java
@@ -0,0 +1,36 @@
+//Write a Java Program to implement inheritance with respect to Interfaces
+
+// Interface Animal
+interface Animal {
+    void eat();
+}
+
+// Interface Pet extends Animal
+interface Pet extends Animal {
+    void play();
+}
+
+// Dog class implementing Pet
+class Dog implements Pet {
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Dog is playing");
+    }
+}
+
+// Main class to test the implementation
+public class Main {
+    public static void main(String[] args) {
+        // Creating an instance of Dog
+        Dog dog = new Dog();
+
+        // Calling methods from the interfaces
+        dog.eat();
+        dog.play();
+    }
+}
